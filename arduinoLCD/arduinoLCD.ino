d0663e49266decc9fda922f82e934200dbc56a30
Levi Pinkard <levipinkard@gmail.com>
2017-01-30 16:31:04 -0500

    Big bot

--- arduinoLCD/arduinoLCD.ino
+++ arduinoLCD/arduinoLCD.ino
@@ -3,7 +3,18 @@
 #include <Wire.h>
 #include <Adafruit_RGBLCDShield.h>
 #include <utility/Adafruit_MCP23017.h>
-
+#include "FastLED.h"
+#define COLOR_ORDER GRB
+#define MAX_BRIGHTNESS 255
+//Tell it how many leds are in the strip. AndyMark's 2.5 meter strip has 150 leds
+#define NUM_LEDS 265
+// This is an array of leds. One item for each led in your strip
+CRGB leds[NUM_LEDS];
+//CSK 3/17/2014 I moved this to a pin that doesn't conflict with Ethernet functions in case you want to controlLEDs via Ethernet
+#define DATA_PIN 6 //White wire from the http://www.andymark.com/product-p/am-2917.htm powerconnector
+//This function is used to setup things like pins, Serial ports etc.
+//Here we specify which chipset our LEDs run off of by our choice of config function
+ CRGB Conductors;
 
 // The shield uses the I2C SCL and SDA pins. On classic Arduinos
 // this is Analog 4 and 5 so you can't use those for analogRead() anymore
@@ -22,34 +33,143 @@ Adafruit_RGBLCDShield lcd = Adafruit_RGBLCDShield();
 
 void setup() {
   // Debugging output
-  Serial.begin(115200);
+  Serial.begin(9600);
   // set up the LCD's number of columns and rows: 
   lcd.begin(16, 2);
   //lcd.print("Conductors 4580");
   lcd.setBacklight(WHITE);
-
+ FastLED.addLeds<WS2812B, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS);
+ FastLED.clear();
+ FastLED.show();
+ delay(250);
+ //clear() turns all LEDs off
+ FastLED.clear();
+ FastLED.setBrightness(MAX_BRIGHTNESS);
+ fill_solid( leds, NUM_LEDS /*number of leds*/, CRGB( 125, 125, 125) );
+ FastLED.show();
+ Conductors.r = 148;
+ Conductors.g = 0;
+ Conductors.b = 211;
 }
-
+void rainbow(uint8_t wait)
+{
+ uint16_t hue;
+ FastLED.clear();
+ for(hue=10; hue<255*3; hue++)
+ {
+ fill_rainbow( &(leds[0]), NUM_LEDS /*led count*/, hue /*starting hue*/);
+ FastLED.show();
+ delay(wait);
+ }
+ return;
+}
+//Cylon - LED sweeps back and forth, with the color, delay and number of cycles of your choice
+void cylon(CRGB color, uint16_t wait, uint8_t number_of_cycles)
+{
+ FastLED.setBrightness(MAX_BRIGHTNESS);
+ for (uint8_t times = 0; times<=number_of_cycles; times++)
+ {
+ // Make it look like one LED is moving in one direction
+ for(int led_number = 0; led_number < NUM_LEDS; led_number++)
+ {
+ //Apply the color that was passed into the function
+ leds[led_number] = color;
+ //Actually turn on the LED we just set
+ FastLED.show();
+ // Turn it back off
+ leds[led_number] = CRGB::Black;
+ // Pause before "going" to next LED
+ delay(wait);
+ }
+ // Now "move" the LED the other direction
+ for(int led_number = NUM_LEDS-1; led_number >= 0; led_number--)
+ {
+ //Apply the color that was passed into the function
+ leds[led_number] = color;
+ //Actually turn on the LED we just set
+ FastLED.show();
+ // Turn it back off
+ leds[led_number] = CRGB::Black;
+ // Pause before "going" to next LED
+ delay(wait);
+ }
+ }
+ return;
+}
 uint8_t i=0;
 void loop() {
-       lcd.print(Serial.readString());
   uint8_t buttons = lcd.readButtons();
-
-  if (buttons) {
-    lcd.setCursor(0,0);
-    if (buttons & BUTTON_UP) {
-      lcd.setBacklight(RED);
-    }
-    if (buttons & BUTTON_DOWN) {
-      lcd.setBacklight(YELLOW);
-    }
-    if (buttons & BUTTON_LEFT) {
-      lcd.setBacklight(GREEN);
-    }
-    if (buttons & BUTTON_RIGHT) {
-    }
-    if (buttons & BUTTON_SELECT) {
-      lcd.clear();
-    }
+      while(true) {
+        String inputSerial = Serial.readString();
+        if (inputSerial == "rainbow") {
+          //cylon(Conductors,0,1);
+          rainbow(1);
+        }
   }
 }
+
+//These are the functions we have defined to do chase patterns. They are actually called inside the loop() above
+//They are meant to demonstrate things such as setting LED colors, controlling brightness
+void color_chase(uint32_t color, uint8_t wait)
+{
+ FastLED.clear();
+ //The brightness ranges from 0-255
+ //Sets brightness for all LEDS at once
+ FastLED.setBrightness(MAX_BRIGHTNESS);
+ // Move a block of LEDs
+
+ for(int led_number = 0; led_number < NUM_LEDS - 5; led_number++)
+ {
+ // Turn our current led ON, then show the leds
+ leds[led_number] = color;
+ //CSK 4/22/2016 Make it multiple dots on
+ leds[led_number + 1] = color;
+ leds[led_number + 2] = color;
+ leds[led_number + 3] = color;
+ leds[led_number + 4] = color;
+ leds[led_number + 5] = color;
+ // Show the leds (only one of which is has a color set, from above
+ // Show turns actually turns on the LEDs
+ FastLED.show();
+ // Wait a little bit
+ delay(wait);
+ // Turn our current led back to black for the next loop around
+ //CSK 4/22/2016 Turn the dots off
+ leds[led_number] = CRGB::Black;
+ }
+ return;
+}
+//Move an "empty" dot down the strip
+void missing_dot_chase(uint32_t color, uint8_t wait)
+{
+ //Step thru some brightness levels from max to 10. led_brightness/=2 is a cryptic shorthand way of sayingled_brightness = led_brightness/2
+ // for (int led_brightness = MAX_BRIGHTNESS; led_brightness > 10; led_brightness/=2)
+ {
+ //FastLED.setBrightness(led_brightness);
+ //CSK 4/22/2016 Turn brightness down to save batteries since almost all leds are on
+ FastLED.setBrightness(25);
+ // Start by turning all pixels on:
+ //for(int led_number = 0; led_number < NUM_LEDS; led_number++) leds[led_number] = color;
+ //https://github.com/FastLED/FastLED/wiki/Controlling-leds
+ fill_solid(leds, NUM_LEDS, color);
+ // Then display one pixel at a time:
+ for(int led_number = 0; led_number < NUM_LEDS - 5; led_number++)
+ {
+ leds[led_number] = CRGB::Black; // Set new pixel 'off'
+ //CSK 4/22/2016
+ leds[led_number + 1] = CRGB::Black; // Set new pixel 'off'
+ leds[led_number + 2] = CRGB::Black; // Set new pixel 'off'
+ leds[led_number + 3] = CRGB::Black; // Set new pixel 'off'
+ leds[led_number + 4] = CRGB::Black; // Set new pixel 'off'
+ leds[led_number + 5] = CRGB::Black; // Set new pixel 'off'
+ if( led_number > 0 && led_number < NUM_LEDS)
+ {
+ leds[led_number-1] = color; // Set previous pixel 'on'
+ }
+ FastLED.show();
+ delay(wait);
+ }
+ }
+ return;
+}
+
--- src/Robot.cpp
+++ src/Robot.cpp
@@ -5,6 +5,7 @@
 #include <Joystick.h>
 #include <CANTalon.h>
 #include <RobotDrive.h>
+#include <Timer.h>
 #include "AHRS.h"
 #include <math.h>
 #include "WPILib.h"
@@ -12,15 +13,18 @@
 #include <LiveWindow/LiveWindow.h>
 #include <SmartDashboard/SendableChooser.h>
 #include <SmartDashboard/SmartDashboard.h>
+#include <DigitalInput.h>
+#include <I2C.h>
 class Robot: public frc::IterativeRobot {
-CANTalon* leftFront;
-CANTalon* leftRear;
-CANTalon* rightFront;
-CANTalon* rightRear;
+CANTalon* leftTop;
+CANTalon* leftBottom;
+CANTalon* rightTop;
+CANTalon* rightBottom;
 RobotDrive *myRobot;
 Joystick* stick;
 Joystick* stick2;
 AHRS* navX;
+Timer timer1;
 bool joystick1_1;
 bool joystick2_1;
 bool joystick3_1;
@@ -51,34 +55,39 @@ double encoderCompensation;
 Encoder *leftEncode;
 Encoder *rightEncode;
 CameraServer* camera;
+SerialPort* arduino;
+DigitalInput* limitOne;
+bool limitInterlock;
+Compressor *mainCompress;
+I2C *rioduino;
 public:
 	void RobotInit() {
-		chooser.AddDefault(autoNameDefault, autoNameDefault);
-		chooser.AddObject(autoNameCustom, autoNameCustom);
+		chooser.AddDefault(autoMiddle, autoMiddle);
+		chooser.AddObject(autoLeft,autoLeft);
 		frc::SmartDashboard::PutData("Auto Modes", &chooser);
 		//Sets Talon SRX CAN IDs
-		leftFront = new CANTalon { 2 };
-		leftRear =  new CANTalon { 3 };
-		rightFront = new CANTalon { 4 };
-		rightRear = new CANTalon { 5 };
+		leftTop = new CANTalon { 1 };
+		leftBottom =  new CANTalon { 0 };
+		rightTop = new CANTalon { 2 };
+		rightBottom = new CANTalon { 3 };
 		//Initiates robot with the 4 Talons
-		myRobot = new RobotDrive {leftFront, leftRear, rightFront, rightRear};
+		myRobot = new RobotDrive { leftTop, leftBottom, rightTop, rightBottom };
 		//Initiates joysticks
 		stick = new Joystick { 0 };
 		stick2 = new Joystick { 1 };
 		//Adds NavX (The large one)
 		navX = new AHRS { SPI::Port::kMXP };
 		navX->Reset();
-		//Adds encoders
-		leftEncode = new Encoder { 2, 3, false, Encoder::EncodingType::k4X};
-		rightEncode = new Encoder { 0, 1, false, Encoder::EncodingType::k4X};
+		leftEncode = new Encoder { 2, 3, false, Encoder::EncodingType::k4X };
+		rightEncode = new Encoder { 0, 1, false, Encoder::EncodingType::k4X };
 		//Encoder compensation to balance out the two sides (One will always go faster)
-		encoderCompensation = 0.94224842556;
-		//Starts camera server
-		camera->GetInstance();
-		camera->StartAutomaticCapture();
+		//arduino = new SerialPort(9600,SerialPort::Port::kUSB,8,SerialPort::kParity_None,SerialPort::kStopBits_One);
+		//CameraServer::GetInstance()->StartAutomaticCapture();
+		encodeDistance = SmartDashboard::PutNumber("1st distance", 111);
+		limitOne = new DigitalInput (6);
+		mainCompress = new Compressor (0);
+		mainCompress->SetClosedLoopControl(true);
 	}
-
 	/*
 	 * This autonomous (along with the chooser code above) shows how to select
 	 * between different autonomous modes using the dashboard. The sendable
@@ -94,45 +103,50 @@ public:
 		autoSelected = chooser.GetSelected();
 		// std::string autoSelected = SmartDashboard::GetString("Auto Selector", autoNameDefault);
 		std::cout << "Auto selected: " << autoSelected << std::endl;
-
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			//Resets all encoder values (so that if teleop was used previously
-			//it won't mess with distance
-			leftEncode->Reset();
-			rightEncode->Reset();
-			//Calculates distance per pulse using wheel diameter * Pi / pulse count (270)
-			encodeDistance = 7.4 * M_PI / 270;
-			leftEncode->SetDistancePerPulse(encodeDistance);
-			rightEncode->SetDistancePerPulse(encodeDistance);
-			//Sets minimum wheel rotation rate before it's considered stopped
-			leftEncode->SetMinRate(.5);
-			rightEncode->SetMinRate(.5);
-			//Robot drives number of inches forward (remember to subtract length of robot)
-			/* while (abs(rightEncode->GetDistance()) <= 27) {
-				myRobot->TankDrive(-1, -1);
-			} */
-			//Rotates bot until it's 180 degrees. About 15 degrees per loop, so subtract that.
-			navX->Reset();
-			while (abs(navX->GetAngle()) < 165) {
-				SmartDashboard::PutNumber("Angle",navX->GetAngle());
-				myRobot->TankDrive(.6,-.6);
+		limitInterlock = true;
+		//Resets all encoder values (so that if teleop was used previously
+		//it won't mess with distance
+		leftEncode->Reset();
+		rightEncode->Reset();
+		//Calculates distance per pulse using wheel diameter * Pi / pulse count (270)
+		encodeDistance = 7.4 * M_PI / 270;
+		leftEncode->SetDistancePerPulse(encodeDistance);
+		rightEncode->SetDistancePerPulse(encodeDistance);
+		//Sets minimum wheel rotation rate before it's considered stopped
+		leftEncode->SetMinRate(.5);
+		rightEncode->SetMinRate(.5);
+		//Robot drives number of inches forward (remember to subtract length of robot)
+		encodeDistance = SmartDashboard::GetNumber("1st distance", 111);
+		while (abs(rightEncode->GetDistance()) <= 27) {
+			myRobot->TankDrive(-.6, -.6);
+			if (limitOne->Get() == true) {
+				limitInterlock = true;
 			}
+			if (limitOne->Get() == false) {
+				leftEncode->Reset();
+				leftEncode->SetDistancePerPulse(encodeDistance);
+				limitInterlock = false;
+				while (abs(leftEncode->GetDistance())  < 10) {
+					myRobot->TankDrive(.6,.6);
+				}
+				Wait(3);
+		}
+		Wait(2);
+		//Rotates bot until it's 180 degrees. About 15 degrees per loop, so subtract that.
+		navX->Reset();
+		while (abs(navX->GetAngle()) < 20) {
+			SmartDashboard::PutNumber("Angle",navX->GetAngle());
+			myRobot->TankDrive(.6,-.6);
+		}
 		}
 	}
 
 	void AutonomousPeriodic() {
-		if (autoSelected == autoNameCustom) {
-			// Custom Auto goes here
-		} else {
-			// Default Auto goes here
-		}
 	}
-
 	void TeleopInit() {
 		interlock = true;
 		slowBool = false;
+		limitInterlock = true;
 	}
 
 	void TeleopPeriodic() {
@@ -159,17 +173,17 @@ public:
 		joystickX_2 = stick2->GetRawAxis(0);
 		//Need interlock to avoid rapid button switching
 		if (joystick3_2 && interlock) {
-			slowBool = true;
+			slowBool = !slowBool;
 			interlock = false;
 		}
 		if (!joystick3_2) {
 			interlock = true;
 		}
 		if (slowBool) {
-			joyLeftValue = joystickY_1 * .5;
-			joyRightValue = joystickY_2 * .5;
+			joyLeftValue = joystickY_1 * .6;
+			joyRightValue = joystickY_2 * .6;
 		} else {
-			joyLeftValue = joystickY_1;
+			joyLeftValue =  joystickY_1;
 			joyRightValue = joystickY_2;
 		}
 		myRobot->TankDrive(joyLeftValue, joyRightValue, true);
@@ -177,6 +191,7 @@ public:
 
 	void TestPeriodic() {
 		lw->Run();
+
 	}
 
 private:
@@ -184,6 +199,9 @@ private:
 	frc::SendableChooser<std::string> chooser;
 	const std::string autoNameDefault = "Default";
 	const std::string autoNameCustom = "My Auto";
+	const std::string autoLeft = "Left";
+	const std::string autoMiddle = "Middle";
+	const std::string autoRight = "Right";
 	std::string autoSelected;
 };
 
